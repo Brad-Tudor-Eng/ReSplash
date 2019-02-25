379424cdfd483cc64590c32c4bb7e05ce92805e5
Brad-Tudor-Eng <brad.tudor.eng@gmail.com>
2019-02-24 17:58:49 -0800

    changed header

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: `https://api.unsplash.com`,
     headers: {
-        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS}`
+        Authorization: `Client-ID 39f6096362aeff39eac373789198e56d69120d181543941f5f132ea087dc632d`
       }
 })
